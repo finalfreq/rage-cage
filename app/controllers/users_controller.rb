f9113dca67ea4e97956fbcf87b45672c242ea734
Alec Arme & James Williams <student@epicodus.com>
2015-07-01 10:25:23 -0700

    Add user edit and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-  
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
 private
   def user_params
     params.require(:user).permit(:name)
