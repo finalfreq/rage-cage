db90b91519165fc95e67a1d4266c719e7e4c1bfc
Alec Arme & James Williams <student@epicodus.com>
2015-07-01 10:16:29 -0700

    Add user new and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,29 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.all  
+    @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:alert] = "Welcome to the Cage"
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
+private
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
